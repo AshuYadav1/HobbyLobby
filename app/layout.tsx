2548f7a52642abc1210562ab64379ac64766d8ec
Aashu Yadav <aashuyadav@Aashus-MacBook-Air.local>
2025-03-21 23:02:12 +0530

    hfidh

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Hobby Lobby',
-  description: 'Created with v0',
+  description: '',
 }
 
 export default function RootLayout({
@@ -13,7 +13,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        {/* Favicon */}
+        <link rel="icon" href="https://i.postimg.cc/439mX8nw/Whats-App-Image-2025-03-19-at-12-17-48-PM-removebg-preview.png" sizes="any" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </head>
+      <body>
+        {children}
+
+        {/* Floating Call Button */}
+        <a 
+          href="tel:+919284017480" 
+          className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition-all duration-300"
+        >
+          📞
+        </a>
+      </body>
     </html>
   )
 }
