92d50461248b3e2fa4ec31a8082f1105b02ee674
Aashu Yadav <aashuyadav@Aashus-MacBook-Air.local>
2025-03-28 12:00:43 +0530

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://hobbylobbyturf.in', // Replace with your actual domain
+    generateRobotsTxt: true, // (Optional) Generates a robots.txt file
+  };
+  
